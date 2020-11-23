61a03ccbc4342f1b94c05563db6452c3e52adb43
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-11-23 23:37:22 +0200

    доделал дз
    добавил всплывающую картинку,  предупреждения при несработке Фетча и пустом ответе на запрос. подрисовал интерфейс

--- src/index.js
+++ src/index.js
@@ -1,47 +1,114 @@
-import './styles.css';
+import './styles.scss';
 import './js-files/apiService.js';
 import  {renderCardMarkup} from './js-files/card-render.js';
 import { error, alert } from '@pnotify/core';
+import"@pnotify/core/dist/PNotify.css";
+import"@pnotify/core/dist/BrightTheme.css";
 import { debounce } from "debounce";
 import { fetchGetCards } from './js-files/apiService';
+import ApiService from './js-files/apiService';
+import * as basicLightbox from 'basiclightbox';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 
-let searchValue = '';
+
+
+const apiService = new ApiService();
+// console.log(screen.height)
 
 const refs = {
-//  markup: document.querySelector('.card-markup'),
+ loadBtn: document.querySelector('[data-action="load-more"]'),
  search: document.querySelector('#search-form'),
  gallery: document.querySelector('.gallery'),
-
-
-
 } ;
 
-refs.search.addEventListener('input',debounce ((inputValue),800));
+refs.search.addEventListener('submit', onSubmit);
+refs.loadBtn.addEventListener('click', onLoadMore);
+// refs.loadBtn.disabled = true;
+refs.loadBtn.hidden = true;
+refs.gallery.addEventListener('click',onImgClick);
 
+// debounce ((onSubmit),900)
 
-function inputValue (event) {
+// console.dir(refs.loadBtn);
+
+function onImgClick(evt) {
+    
+   const src = evt.target.dataset.src; 
+   if (src) {
+    console.log(src);
+    const instance = basicLightbox.create(`
+    <img class = “imgModal” src=${src} width=“800” height=“600">`);
+    //  console.dir(instance);
+     instance.show();
+   };
+   
+
+}
+
+function resetMarkup () {
+    refs.gallery.innerHTML = '';  
+    
+}
+
+function onSubmit (event) {
+   
     event.preventDefault();
-    searchValue = event.target.value;
-    // console.log(searchValue);
-    fetchOnInput(searchValue);
-};
-
-function fetchOnInput (searchValue) {
-    fetchGetCards(searchValue).then(data => {
-        console.log(data.hits);
-        renderCardMarkup(data.hits,(refs.gallery));
-    });
-//    console.log(renderCardMarkup);
+    apiService.resetPage();
+    resetMarkup ();
+    apiService.value = event.currentTarget.elements.query.value;
+    fetchValue();
     
 };
+ 
+async function fetchValue () {
+    try {
+      await apiService.fetchGetCards().then(hits => {
+            if (hits.length > 0) {
+        
+        renderCardMarkup(hits,(refs.gallery));
+        // console.log(hits);
+        refs.loadBtn.disabled = false;
+        refs.loadBtn.hidden = false;
+        // scrollTo();
+        
+            } else {
+                refs.loadBtn.hidden = true;
+                errorMessage('По вашему запросу ничего не найдено')
+            }
+      });
+      scrollTo();
+    //   refs.loadBtn.disabled = false;
+    } catch  {
+        refs.loadBtn.hidden = true;
+        errorMessage("Все сломалось в Fetch запрсе! Попробуйте позже или обратитесь к администратору!");
+    }
+};
+
+function errorMessage (message) {
+    const myError = error({
+    text: message,
+    hide: false,
+    delay:1500,
+    });
+    setTimeout(() => {
+        myError.hide = true;
+    }, 200);
+
+};
 
 
-// renderSearchForm();
+function onLoadMore(evt) {
+    // scrollTo();
+    fetchValue();
+};
 
+function scrollTo () {
+    const height = window.screen.height;
+    const scrollHeight = window.pageYOffset;
+    scroll = height + scrollHeight;
+    window.scrollTo({
+        top: scroll,
+        behavior: "smooth",
+    });
+}
 
-// const KEY = '19207459-7123426cd96bb4a7307e45de7';
-// //   https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
-// const search = 'cat'
-
-// fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=${page}&per_page=12&key=${KEY}`)
-// .then(res => res.json).then(console.log);
--- src/js-files/apiService.js
+++ src/js-files/apiService.js
@@ -1,28 +1,35 @@
 const KEY = '19207459-7123426cd96bb4a7307e45de7';
-//   https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
-// const search = 'cat'
+const URL = 'https://pixabay.com/api/?image_typr=photo&orientation=horizontal&q=';
 
-const URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=';
-let page = 1;
+export default class ApiService {
+    constructor () {
+        this.searchValue = '';
+        this.page = 1;
 
-export const fetchGetCards = async (search) => {
-    try {
-        const card = await fetch(`${URL}${search}&page=${page}&per_page=12&key=${KEY}`);
-        const fotos = card.json();
-        // console.log(fotos);
-        return fotos;
-    } catch (error) {
-        error('Опаньки что-то пошло не так!!')
-        
-    };
+    }
+    async fetchGetCards () {
+        return fetch(`${URL}${this.searchValue}&page=${this.page}&per_page=12&key=${KEY}`)
+        .then(response =>response.json())
+        .then(({hits}) =>{
+            this.page+=1;
+            return hits;
+        });
     
-}
- 
+        
+    }
+    get value () {
+        return this.searchValue;
 
-// function fetch (url,searh,key) {
-//     fetch(`${URL}${search}&page=${page}&per_page=12&key=${KEY}`).then(res => res.json).then(console.log);
-// }
-// 
+    }
+    set value (newValue) {
+        this.searchValue = newValue;
+    }
+    resetPage(){
+        this.page =1;
+    }
+}
+
+
 
  
 
